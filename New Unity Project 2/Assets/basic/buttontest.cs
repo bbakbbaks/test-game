0772afb12312a1020ca449db48ba6a8c2140a448
bbakbbaks <ehd0825@gmail.com>
2018-04-26 12:54:00 +0900

    scrollview&roomexit
    4.26

---
+++ New Unity Project 2/Assets/basic/buttontest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttontest : MonoBehaviour {
+    public Text m_cText;
+    Button m_cButton;
+	// Use this for initialization
+    public 
+	void Start () {
+        //오브젝트로 부터 필요한 컴포넌트를 받아오려면 GetComponent를 이용한다,
+        m_cButton = this.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickEvent()
+    {
+        m_cText.text = "test";
+        m_cButton.colors = ColorBlock.defaultColorBlock;
+    }
+}
